e279ea5d1c46e6ff6c3879ab469363623316f2bc
Nwokafor Chukwuebuka <nwokafor.choongsaeng@turntabl.io>
2021-11-19 15:35:43 +0000

    overload popCard Method in Deck

--- src/main/scala/blackjack/Deck.scala
+++ src/main/scala/blackjack/Deck.scala
@@ -12,5 +12,16 @@ class Deck {
    */
   def popCard = deck.remove(deck.size - 1)
 
+  /**
+   * removes and returns the a specified number of cards from the top of the stack
+   * @return List[Card]
+   */
+  def popCard(count: Int) = {
+    val split = deck.splitAt(deck.size - count)
+    deck = split._1
+    split._2.toList
+  }
+
+
   def shuffleDeck:Unit = deck = shuffle(deck)
 }
