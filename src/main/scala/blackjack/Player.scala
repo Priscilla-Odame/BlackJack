0802b475feaef192b2ad15251806c7eca9b13290
Nwokafor Chukwuebuka <nwokafor.choongsaeng@turntabl.io>
2021-11-19 15:12:28 +0000

    overload collect method in Player

--- src/main/scala/blackjack/Player.scala
+++ src/main/scala/blackjack/Player.scala
@@ -3,12 +3,14 @@ package blackjack
 import scala.collection.mutable.ListBuffer
 
 class Player(val name:String, private val strategy: Strategy = new DefaultStrategy()) {
-  private val cards:ListBuffer[Card] = ListBuffer()
+  private val cardsInHand:ListBuffer[Card] = ListBuffer()
 
   // a player shouldn't have the same card twice
-  def collect(card: Card): Unit = if(!cards.contains(card)) cards.append(card)
+  def collect(cardGiven: Card): Unit = if(!cardsInHand.contains(cardGiven)) cardsInHand.append(cardGiven)
+
+  def collect(cardsGiven: List[Card]): Unit = cardsGiven.foreach(card => if(!cardsInHand.contains(card)) cardsInHand.append(card))
 
   def playTurn: String = strategy.useStrategy(getTotalCardValue)
 
-  def getTotalCardValue = cards.map(_.value).reduce(_ + _)
+  def getTotalCardValue = cardsInHand.map(_.value).reduce(_ + _)
 }
